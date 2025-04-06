c710dd957e6f3e0c65fa2c90bd3d9c41cdd03f2a
Prajwal Mogaveera <prmm98@gmail.com>
2025-04-06 21:24:29 +0530

    .py formatter

--- lua/plugins/autoformat.lua
+++ lua/plugins/autoformat.lua
@@ -30,6 +30,7 @@ return {
       lua = { 'stylua' },
       -- Conform can also run multiple formatters sequentially
       python = { 'isort', 'black' },
+      py = { 'isort', 'black' },
       --
       -- You can use a sub-list to tell conform to run *until* a formatter
       -- is found.
