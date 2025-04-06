2c287db3b7b23e915da570b473b5804291e20ca6
Prajwal Mogaveera <prmm98@gmail.com>
2025-04-06 21:20:01 +0530

    python formatter

--- lua/plugins/debug.lua
+++ lua/plugins/debug.lua
@@ -22,7 +22,7 @@ return {
     'jay-babu/mason-nvim-dap.nvim',
 
     -- Add your own debuggers here
-    'leoluz/nvim-dap-go',
+    -- 'leoluz/nvim-dap-go',
   },
   config = function()
     local dap = require 'dap'
@@ -41,7 +41,9 @@ return {
       -- online, please don't ask me how to install them :)
       ensure_installed = {
         -- Update this to ensure that you have the debuggers for the langs you want
-        'delve',
+        -- 'delve',
+        'java-debug-adapter',
+        'java-test',
       },
     }
 
@@ -101,12 +103,12 @@ return {
     }
 
     -- Install golang specific config
-    require('dap-go').setup {
-      delve = {
-        -- On Windows delve must be run attached or it crashes.
-        -- See https://github.com/leoluz/nvim-dap-go/blob/main/README.md#configuring
-        detached = vim.fn.has 'win32' == 0,
-      },
-    }
+    -- require('dap-go').setup {
+    --   delve = {
+    --     -- On Windows delve must be run attached or it crashes.
+    --     -- See https://github.com/leoluz/nvim-dap-go/blob/main/README.md#configuring
+    --     detached = vim.fn.has 'win32' == 0,
+    --   },
+    -- }
   end,
 }
--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -198,8 +198,6 @@ return {
     local ensure_installed = vim.tbl_keys(servers or {})
     vim.list_extend(ensure_installed, {
       'stylua', -- Used to format Lua code
-      'java-debug-adapter',
-      'java-test',
     })
     require('mason-tool-installer').setup { ensure_installed = ensure_installed }
 
